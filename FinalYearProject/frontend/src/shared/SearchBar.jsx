d6e8233661555ac3dc764aa79d2ebe1c9027f065
Aisharya Roy <aisharyaroy8420284490@gmail.com>
2024-05-18 13:21:26 +0530

    Aisharya

--- FinalYearProject/frontend/src/shared/SearchBar.jsx
+++ FinalYearProject/frontend/src/shared/SearchBar.jsx
@@ -23,59 +23,52 @@ const SearchBar = () =>{
 
 
     return (
-        // <div className="entire__search__bar">
-        <Col lg="12"> 
+
+        <Col lg="12">
             <div className="search__bar">
-                 <Form className="d-flex align-items-center gap-4 cust-form ">
+                <Form className="d-flex align-items-center gap-4 cust-form">
                     <FormGroup className="d-flex gap-3 form__group form__group-fast">
-                         <span>
-                         <i class="ri-map-pin-line"></i>
-                         </span>
-                         <div>
+                        <span><i class="ri-map-pin-line"></i></span>
+                        <div>
                             <h6>From where?</h6>
                             <input type="text" placeholder="Departure Depot" ref={fromRef} />
-                         </div>
+                        </div>
                     </FormGroup>
 
                     <FormGroup className="d-flex gap-3 form__group form__group-fast">
-                         <span>
-                         <i class="ri-map-pin-line"></i>
-                         </span>
-                         <div>
+                        <span><i class="ri-map-pin-line"></i></span>
+                        <div>
                             <h6>Where to?</h6>
                             <input type="text" placeholder="Destination Depot" ref={toRef} />
-                         </div>
+                        </div>
                     </FormGroup>
 
                     <FormGroup className="d-flex gap-3 form__group form__group-fast">
-                         <span>
-                         <i class="ri-calendar-event-line"></i>
-                         </span>
-                         <div>
+                        <span><i class="ri-calendar-event-line"></i></span>
+                        <div>
                             <h6>Date</h6>
-                            <input type="date" ref={dateRef} /> 
-                         </div>
+                            <input type="date" ref={dateRef} />
+                        </div>
                     </FormGroup>
 
                     <FormGroup className="d-flex gap-3 form__group form__group-last">
-                         <span>
-                         <i class="ri-bus-wifi-line"></i>
-                         </span>
-                         <div>
+                        <span><i class="ri-bus-wifi-line"></i></span>
+                        <div>
                             <h6>Bus Type</h6>
-                            <input type="text" placeholder="Select Bus" ref={busRef}/>
-                         </div>
+                            <input type="text" placeholder="Select Bus" ref={busRef} />
+                        </div>
                     </FormGroup>
 
-                    <span className="search__icon" type ="submit" onClick={searchHandler} >
+                    <span className="search__icon" type="submit" onClick={searchHandler}>
                         <i class="ri-search-line"></i>
                     </span>
-
-                 </Form>
+                </Form>
             </div>
         </Col>
-        // </div>
-    )
+
+
+
+)
     
 };
 
